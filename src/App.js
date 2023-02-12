3b8d66e26e60b3aac2657adb93b1d86d52264b80
choi ha young <gkdudk15@naver.com>
2023-02-12 20:23:45 +0900

    feat: 스타일드 컨포넌트 실습2

--- src/App.js
+++ src/App.js
@@ -1,21 +1,40 @@
 import './App.css';
 import styled from 'styled-components';
 
+const StContainer = styled.div`
+    display: flex;
+`;
+
 const StBox = styled.div`
     width: 100px;
     height: 100px;
     border: 1px solid ${(props) => props.borderColor};
     margin: 20px;
-    background-color: ${(props) => props.backgroundColor};
 `;
+
+// 박스의 색
+const boxList = ['red', 'blue', 'green'];
+
+const getBoxName = (color) => {
+    switch (color) {
+        case 'red':
+            return '빨간 박스';
+        case 'green':
+            return '초록 박스';
+        case 'blue':
+            return '파란 박스';
+        default:
+            return '검정 박스';
+    }
+};
 
 function App() {
     return (
-        <>
-            <StBox borderColor="red">123</StBox>
-            <StBox borderColor="blue">1263</StBox>
-            <StBox borderColor="green">123</StBox>
-        </>
+        <StContainer>
+            {boxList.map((box) => {
+                return <StBox borderColor={box}>{getBoxName(box)}</StBox>;
+            })}
+        </StContainer>
     );
 }
 
