628e6b7ce8efb8837ab8cc208aa3f9d0de7a6e3d
choi ha young <gkdudk15@naver.com>
2023-02-12 19:12:09 +0900

    feat: 스타일드 컨포넌트 실습1

--- src/App.js
+++ src/App.js
@@ -4,19 +4,18 @@ import styled from 'styled-components';
 const StBox = styled.div`
     width: 100px;
     height: 100px;
-    border: 1px solid red;
+    border: 1px solid ${(props) => props.borderColor};
     margin: 20px;
-`;
-
-const StP = styled.p`
-    color: blue;
+    background-color: ${(props) => props.backgroundColor};
 `;
 
 function App() {
     return (
-        <StBox>
-            <StP>나는 박스입니다</StP>
-        </StBox>
+        <>
+            <StBox borderColor="red">123</StBox>
+            <StBox borderColor="blue">1263</StBox>
+            <StBox borderColor="green">123</StBox>
+        </>
     );
 }
 
